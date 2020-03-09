68e67978573fd7f331228a016af04f781129e2d6
David Kogan <kogand2@mcmaster.ca>
2020-03-09 04:21:30 -0400

    Updates

---
+++ precache-manifest.0c9db4da1540472f44c2614c10e174c3.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc8357ea055afd8836e6ca436f7e7841",
+    "url": "/nbalivestats/index.html"
+  },
+  {
+    "revision": "b10f51d9720c5ef3fe12",
+    "url": "/nbalivestats/static/css/main.7d466ccc.chunk.css"
+  },
+  {
+    "revision": "5c07d697933e096b69e7",
+    "url": "/nbalivestats/static/js/2.3dfb5f0a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/nbalivestats/static/js/2.3dfb5f0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b10f51d9720c5ef3fe12",
+    "url": "/nbalivestats/static/js/main.8266f849.chunk.js"
+  },
+  {
+    "revision": "af5524839ed136ab8c4b",
+    "url": "/nbalivestats/static/js/runtime-main.577b4bc4.js"
+  },
+  {
+    "revision": "04457bc5b0245219a56c65a30e359e15",
+    "url": "/nbalivestats/static/media/76ers.04457bc5.png"
+  },
+  {
+    "revision": "ac3c98325b68e4e8d5366f77ec53d718",
+    "url": "/nbalivestats/static/media/blazers.ac3c9832.png"
+  },
+  {
+    "revision": "6daa5165dad8adfb44fa377ccfb62ed3",
+    "url": "/nbalivestats/static/media/bucks.6daa5165.png"
+  },
+  {
+    "revision": "319eb0112425b44ffadad85b300fd035",
+    "url": "/nbalivestats/static/media/bulls.319eb011.png"
+  },
+  {
+    "revision": "bb4f43513ddca1f1894b6d60d9c7d3e7",
+    "url": "/nbalivestats/static/media/cavs.bb4f4351.png"
+  },
+  {
+    "revision": "a134ea0003ce24811cfb87b7375f37dd",
+    "url": "/nbalivestats/static/media/celtics.a134ea00.png"
+  },
+  {
+    "revision": "71d9d1f1432447051892d6e919534d7c",
+    "url": "/nbalivestats/static/media/clippers.71d9d1f1.png"
+  },
+  {
+    "revision": "2a750344bee68de76383582290d96cf5",
+    "url": "/nbalivestats/static/media/grizzlies.2a750344.png"
+  },
+  {
+    "revision": "e1b34c6d98a2579b625ee27e792ba5db",
+    "url": "/nbalivestats/static/media/hawks.e1b34c6d.png"
+  },
+  {
+    "revision": "f907ade6c6bac2f52cd9eaf0bc186ba3",
+    "url": "/nbalivestats/static/media/heat.f907ade6.png"
+  },
+  {
+    "revision": "c27c1fdc695ba2ad3d787e7dd0ce66a4",
+    "url": "/nbalivestats/static/media/hornets.c27c1fdc.png"
+  },
+  {
+    "revision": "cffb2e54d1094a8bf7d28620a2504cb0",
+    "url": "/nbalivestats/static/media/jazz.cffb2e54.png"
+  },
+  {
+    "revision": "989bba2eb40f150bff8026ff7a8c47d4",
+    "url": "/nbalivestats/static/media/kings.989bba2e.png"
+  },
+  {
+    "revision": "a35202fcfd29b6fac946550036fa9319",
+    "url": "/nbalivestats/static/media/knicks.a35202fc.png"
+  },
+  {
+    "revision": "f27bcbfbb71a14e0a870d27ee89accb4",
+    "url": "/nbalivestats/static/media/lakers.f27bcbfb.png"
+  },
+  {
+    "revision": "3e14f48027af6dde228b141d7f56f80b",
+    "url": "/nbalivestats/static/media/magic.3e14f480.png"
+  },
+  {
+    "revision": "f0614f5841cb21984670bc30375a4a10",
+    "url": "/nbalivestats/static/media/mavs.f0614f58.png"
+  },
+  {
+    "revision": "8066ebc3fbc1295adb975617311ba8c6",
+    "url": "/nbalivestats/static/media/nets.8066ebc3.png"
+  },
+  {
+    "revision": "f42712216723f8a560d4b57f4a6a5cc5",
+    "url": "/nbalivestats/static/media/nuggets.f4271221.png"
+  },
+  {
+    "revision": "9629bff925dfff859c8b04f07c40a058",
+    "url": "/nbalivestats/static/media/pacers.9629bff9.png"
+  },
+  {
+    "revision": "cf6620196217fd40310e2239a2621caa",
+    "url": "/nbalivestats/static/media/pelicans.cf662019.png"
+  },
+  {
+    "revision": "129b7012c426f8e8ac6cd1c1a560fc22",
+    "url": "/nbalivestats/static/media/pistons.129b7012.png"
+  },
+  {
+    "revision": "5e76e4655faab33a95e47cad46adcf98",
+    "url": "/nbalivestats/static/media/raptors.5e76e465.png"
+  },
+  {
+    "revision": "8257a19b2dc4d441594fce2a6881d12f",
+    "url": "/nbalivestats/static/media/rockets.8257a19b.png"
+  },
+  {
+    "revision": "7dbb30c69fe06f79f97e969d163f1e9a",
+    "url": "/nbalivestats/static/media/spurs.7dbb30c6.png"
+  },
+  {
+    "revision": "300133ca6edca9d4a438c9391e036fea",
+    "url": "/nbalivestats/static/media/suns.300133ca.png"
+  },
+  {
+    "revision": "ab526cf33285d6acefae236752bc8aff",
+    "url": "/nbalivestats/static/media/thunder.ab526cf3.png"
+  },
+  {
+    "revision": "3756195b0e96acbffe5ad6a50702fe26",
+    "url": "/nbalivestats/static/media/warriors.3756195b.png"
+  },
+  {
+    "revision": "7f8d02d01d0e3501ae5a3c43554a7972",
+    "url": "/nbalivestats/static/media/wizards.7f8d02d0.png"
+  },
+  {
+    "revision": "05a946834e87ec66c3309d7c15acfc14",
+    "url": "/nbalivestats/static/media/wolves.05a94683.png"
+  }
+]);
